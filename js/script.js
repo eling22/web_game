426d1c0e502892aa17902cc92e7dfa1e1fbe6273
eling <lai.eling22@gmail.com>
2020-03-24 18:46:13 +0800

    bounce off the walls

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ var ctx = canvas.getContext("2d");
 
 var x = canvas.width / 2;
 var y = canvas.height - 30;
+var ballRadius = 10;
 var dx = 2, dy = -2;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 20, 0, Math.PI * 2, false);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2, false);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -17,6 +18,9 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     x += dx; y += dy;
+    //boundary
+    if (x + dx - ballRadius < 0 || x + dx + ballRadius > canvas.width) dx = -dx;
+    if (y + dy - ballRadius < 0 || y + dy + ballRadius > canvas.height) dy = -dy;
 }
 
 setInterval(draw, 10);
