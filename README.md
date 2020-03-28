0e6a9e55bd87cbefe850c3fe77fe1d6d3c8bce35
eling <lai.eling22@gmail.com>
2020-03-28 19:48:48 +0800

    brick collision detection

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ the page show on https://eling22.github.io/web_game/
 # next step
 * Breakout
   * following the tutorial steps
-    * [Collision detection](https://developer.mozilla.org/en-US/docs/Games/Workflows/2D_Breakout_game_pure_JavaScript/Collision_detection)
     * [Track the score and win](https://developer.mozilla.org/en-US/docs/Games/Workflows/2D_Breakout_game_pure_JavaScript/Track_the_score_and_win)
     * [Mouse controls](https://developer.mozilla.org/en-US/docs/Games/Workflows/2D_Breakout_game_pure_JavaScript/Mouse_controls)
     * [Finishing up](https://developer.mozilla.org/en-US/docs/Games/Workflows/2D_Breakout_game_pure_JavaScript/Finishing_up)
--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+function isInRange(x, min, max) {
+    return x > min && x < max;
+}
+
 function Ball() {
     this.x = canvas.width / 2;
-    this.y = canvas.height - 30;
+    this.y = canvas.height - 50;
     this.radius = 10;
     this.dx = 2;
     this.dy = -2;
@@ -23,7 +27,9 @@ function Ball() {
             left: this.x + this.dx - this.radius,
             right: this.x + this.dx + this.radius,
             top: this.y + this.dy - this.radius,
-            bottom: this.y + this.dy + this.radius
+            bottom: this.y + this.dy + this.radius,
+            x: this.x + this.dx,
+            y: this.y + this.dy
         };
     };
     this.boundaryBounce = function (canvas) {
@@ -39,16 +45,45 @@ function Ball() {
             clearInterval(interval);
         }
     };
-    this.collision = function (paddle) {
-        var pos = paddle.getPos();
+    this.collisionObject = function (obj_pos, ball_next_pos) {
+        if (isInRange(ball_next_pos.x, obj_pos.x1, obj_pos.x2)) {
+            if (isInRange(ball_next_pos.bottom, obj_pos.y1, obj_pos.y2) ||
+                isInRange(ball_next_pos.top, obj_pos.y1, obj_pos.y2)) {
+                this.dy = -this.dy;
+                return true;
+            }
+        }
+        if (isInRange(ball_next_pos.y, obj_pos.y1, obj_pos.y2)) {
+            if (isInRange(ball_next_pos.left, obj_pos.x1, obj_pos.x2) ||
+                isInRange(ball_next_pos.right, obj_pos.x1, obj_pos.x2)) {
+                this.dx = -this.dx;
+                return true;
+            }
+        }
+        return false;
+    };
+    this.collisionDetection = function(paddle,brick){
         var npos = this.nextPos();
-        if (npos.bottom > pos.y1 && this.x > pos.x1 && this.x < pos.x2) this.dy = -this.dy;
-        //console.log()
+        //paddle
+        var pos = paddle.getPos();
+        this.collisionObject(pos, npos);
+        //cancel the effect of bounce from paddle bottom
+        if (isInRange(npos.x, pos.x1, pos.x2) && isInRange(npos.top, pos.y1, pos.y2)) this.dy = -this.dy;
+        //brick
+        for (var i = 0; i < brick.col_count; i++) {
+            for (var j = 0; j < brick.row_count; j++) {
+                if (brick.bricks[i][j].status == 1) {
+                    var is_collision = this.collisionObject(brick.bricks[i][j], npos);
+                    if (is_collision) brick.bricks[i][j].status = 0;
+                }
+            }
+        }
     };
 }
 function Paddle() {
     this.width = 75;
     this.height = 10;
+    this.offset_bottom = 30;
     this.x = canvas.width / 2;
     this.speed = 7;
     this.rightPress = false;
@@ -58,7 +93,7 @@ function Paddle() {
 
     this.draw = function () {
         ctx.beginPath();
-        var start_y = canvas.height - this.height - 10;
+        var start_y = canvas.height - this.height - this.offset_bottom;
         ctx.rect(this.x - halfwidth, start_y, this.width, this.height);
         ctx.fillStyle = "#0095DD";
         ctx.fill();
@@ -74,8 +109,8 @@ function Paddle() {
         return {
             x1: this.x - halfwidth,
             x2: this.x + halfwidth,
-            y1: canvas.height - this.height - 10,
-            y2: canvas.height - 10
+            y1: canvas.height - this.height - this.offset_bottom,
+            y2: canvas.height - this.offset_bottom
         };
     };
 
@@ -88,30 +123,30 @@ function Brick() {
     this.padding = 10;
     this.offset_top = 30;
     this.offset_left = 30;
-    var bricks = [];
+    this.bricks = [];
     for (var i = 0; i < this.col_count; i++) {
-        bricks[i] = [];
+        this.bricks[i] = [];
         for (var j = 0; j < this.row_count; j++) {
-            bricks[i][j] = { x: 0, y: 0 };
+            this.bricks[i][j] = { x1: 0, y1: 0, x2: 0, y2: 0, status: 1 };
+            //status { 0: not exist, 1:exist} 
+            this.bricks[i][j].x1 = this.offset_left + i * (this.width + this.padding);
+            this.bricks[i][j].x2 = this.bricks[i][j].x1 + this.width;
+            this.bricks[i][j].y1 = this.offset_top + j * (this.height + this.padding);
+            this.bricks[i][j].y2 = this.bricks[i][j].y1 + this.height;
         }
     }
     this.draw = function () {
         for (var i = 0; i < this.col_count; i++) {
             for (var j = 0; j < this.row_count; j++) {
-                bricks[i][j].x = this.offset_left + i * (this.width + this.padding);
-                bricks[i][j].y = this.offset_top + j * (this.height + this.padding);
-                ctx.beginPath();
-                ctx.rect(bricks[i][j].x, bricks[i][j].y, this.width, this.height);
-                ctx.fillStyle = "#0095DD";
-                ctx.fill();
-                ctx.closePath();
+                if (this.bricks[i][j].status == 1) {
+                    ctx.beginPath();
+                    ctx.rect(this.bricks[i][j].x1, this.bricks[i][j].y1, this.width, this.height);
+                    ctx.fillStyle = "#0095DD";
+                    ctx.fill();
+                    ctx.closePath();
+                }
             }
         }
-        ctx.beginPath();
-        //ctx.rect(this.x - halfwidth, start_y, this.width, this.height);
-        ctx.fillStyle = "#0095DD";
-        //ctx.fill();
-        ctx.closePath();
     };
 }
 
@@ -149,7 +184,7 @@ function draw() {
     ball.move();
     paddle.move();
     ball.boundaryBounce(canvas);
-    ball.collision(paddle);
+    ball.collisionDetection(paddle,brick);
     ball.gameOver();
 }
 
